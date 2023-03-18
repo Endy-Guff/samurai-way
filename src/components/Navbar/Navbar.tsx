af2a83769872659d23d525181f8f64e6990ed48a
Andrey <andreymaranchak@yandex.ru>
2023-03-18 23:01:01 +0300

    20 lesson

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 export function Navbar(){
     return <nav className={s.nav}>
     <ul className={s.nav_list}>
-      <li className={s.nav_item}><a href="/Profile">Profile</a></li>
-      <li className={s.nav_item}><a href="/Messages">Messages</a></li>
+      <li className={s.nav_item}><NavLink to="/Profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink></li>
+      <li className={s.nav_item}><NavLink to="/Messages" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink></li>
       <li className={s.nav_item}>News</li>
       <li className={s.nav_item}>Music</li>
       <li className={s.nav_item}>Settings</li>
