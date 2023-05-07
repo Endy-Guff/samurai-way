4d59af07d5a766609dbd06091d51ccf7ce165f1b
Endy-Guff <andreymaranchak@yandex.ru>
2023-05-07 21:44:16 +0300

    53 lesson

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -4,44 +4,43 @@ import {MapToPropsType} from "./UsersContainer";
 import {instance} from "../../redux/reduxStore";
 import localPhoto from "../../assets/img/user.png"
 
-export const Users: React.FC<MapToPropsType> = (
-    {
-        state,
-        follow,
-        unfollow,
-        setUsers
-    }
-) =>{
-
-    if(state.length === 0){
-        debugger
-        instance.get('https://social-network.samuraijs.com/api/1.0/users').then(response=>setUsers(response.data.items))
+export class Users extends React.Component<MapToPropsType> {
+    constructor(props: MapToPropsType) {
+        super(props);
+        instance.get('https://social-network.samuraijs.com/api/1.0/users').then(response=>this.props.setUsers(response.data.items))
     }
 
 
-    const mappedUsers = state.map(u=>{return(
-        <div className={s.user} key={u.id}>
-            <div className={s.inform}>
-                <div className={s.avaBlock}>
-                    <img className={s.img} src={u.photos.small!=null?u.photos.small:localPhoto} alt=""/>
-                </div>
-                <div className={s.desc}>
-                    <span className={s.name}>{u.name}</span>
-                    <span className={s.status}>{u.status}</span>
-                </div>
-            </div>
-            <div className={s.btnBox}>
-                {u.followed
-                    ?<button className={s.btn} onClick={()=>unfollow(u.id)}>unfollow</button>
-                    :<button className={s.btn} onClick={()=>follow(u.id)}>follow</button>
-                }
-            </div>
-        </div>
-    )})
 
+render()
+{
     return (
         <div className={s.wrapper}>
-            {mappedUsers}
+            {
+                this.props.state.map(u => {
+                    return (
+                        <div className={s.user} key={u.id}>
+                            <div className={s.inform}>
+                                <div className={s.avaBlock}>
+                                    <img className={s.img} src={u.photos.small != null ? u.photos.small : localPhoto}
+                                         alt=""/>
+                                </div>
+                                <div className={s.desc}>
+                                    <span className={s.name}>{u.name}</span>
+                                    <span className={s.status}>{u.status}</span>
+                                </div>
+                            </div>
+                            <div className={s.btnBox}>
+                                {u.followed
+                                    ? <button className={s.btn} onClick={() => this.props.unfollow(u.id)}>unfollow</button>
+                                    : <button className={s.btn} onClick={() => this.props.follow(u.id)}>follow</button>
+                                }
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
+}
 }
